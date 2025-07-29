89108a759218c65151bfafb3d32adfa4032aa397
retalily <75236755+retalily@users.noreply.github.com>
2025-07-30 00:14:05 +0200

    Update gmal_legal_assistant_rag_final.py

--- gmal_legal_assistant_rag_final.py
+++ gmal_legal_assistant_rag_final.py
@@ -5,6 +5,7 @@ import requests
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.chat_models import ChatOpenAI
 from langchain_community.document_loaders import TextLoader
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
